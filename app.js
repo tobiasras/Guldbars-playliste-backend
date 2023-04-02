9e5dcce115e58c2de48dd0ac1961bafd66958fc4
tobiasras <tobiasras.job@gmail.com>
2023-04-03 01:46:05 +0200

    added cors

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ import * as dotenv from 'dotenv'
 import spotifyToken from './spotify/spotifyToken.js'
 
 const app = express()
+app.use(express.json())
+
 dotenv.config()
+// api
+app.get('/authorizeLogin/:password', (req, res) => {
+  if (req.query.password) {
+    console.log(req.query.password)
+  }
+  res.send(JSON.stringify({ hello: 'hello from backend' }))
+})
 
 app.get('/login', function (req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
@@ -19,9 +28,6 @@ app.get('/login', function (req, res) {
 
 app.get('/callback', async (req, res) => {
   const code = req.query.code || null
-
-  console.log('is logged in')
-
   const body = new URLSearchParams()
   body.append('grant_type', 'authorization_code')
   body.append('code', code)
@@ -30,19 +36,18 @@ app.get('/callback', async (req, res) => {
   const spotifyAccess = await spotifyToken(body)
 
   setupAccessToken(spotifyAccess)
-
-  res.redirect('/')
+  res.redirect('/load')
 })
 
-app.get('/next', async (req, res) => {
-  const response = await fetch('https://api.spotify.com/v1/me/player/next', {
+// api
+app.get('/api/next', async (req, res) => {
+  fetch('https://api.spotify.com/v1/me/player/next', {
     method: 'POST',
     headers: {
       Authorization: 'Bearer ' + await getAccessToken()
     }
   })
-
-  res.send('switch')
+  res.send()
 })
 
 app.listen(8080, (error) => {
